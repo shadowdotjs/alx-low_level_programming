df5fc22e2edeb10b5c937972f4a730a676b9a56f
Saheed Olurebi <saheedolurebi40@gmail.com>
2023-02-16 15:56:19 -0800

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is the main function used to print a string using printf
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
