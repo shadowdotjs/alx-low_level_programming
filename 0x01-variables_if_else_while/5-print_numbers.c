38b8c6381764c10049c12fe9ff882699666bd8de
Saheed Olurebi <saheedolurebi40@gmail.com>
2023-02-17 16:22:16 -0800

    binary

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* Positive anything is better than negative nothing */
+/**
+ * main- entry point for program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", i);
+	}
+		printf("\n");
+		return (0);
+}
