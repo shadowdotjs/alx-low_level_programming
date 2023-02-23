1ad8c851503fd9097fa756ad2ecd6255022d0022
Saheed Olurebi <saheedolurebi40@gmail.com>
2023-02-23 15:08:33 -0800

    multiply

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,18 +1,16 @@
 #include "main.h"
-
 /**
- * print_numbers - checks for checks for a digit (0 through 9).
- *
+ * mul - checks for checks for a digit (0 through 9).
+ * @a: a - Variable
+ * @b: b - variable
  * Return: Always 0.
  */
-void print_numbers(void)
+int mul(int a, int b)
 {
-int c;
+int mul;
 
-for (c = 48; c < 58; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+mul = a * b;
+
+return (mul);
 }
 
