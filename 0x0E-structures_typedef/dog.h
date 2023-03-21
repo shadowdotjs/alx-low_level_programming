d4a62ff75ece934a6d70b4743417343c6d3f8497
Saheed Olurebi <saheedolurebi40@gmail.com>
2023-03-21 14:31:04 -0700

    struct and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,14 +2,14 @@
 #define DOG_H
 
 
-	/**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 	struct dog
 	{
 		char *name;
@@ -18,9 +18,9 @@
 	};
 
 
-	/**
-	 * dog_t - typedef for struct dog
-	 */
+/**
+ * dog_t - typedef for struct dog
+ */
 	typedef struct dog dog_t;
 
 
@@ -32,5 +32,5 @@
 	int _strlen(char *s);
 
 
-	#endif
+#endif
 
